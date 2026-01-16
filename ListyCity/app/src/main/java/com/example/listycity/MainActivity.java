b8049bdb0ed0e07cf430ceb56e8129094bc53b5d
SamiPokh <Samippya@ualberta.ca>
2026-01-16 15:01:57 -0700

    finished

---
+++ ListyCity/app/src/main/java/com/example/listycity/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.listycity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView cityList;
+    ArrayAdapter<String> cityAdapter;
+    ArrayList<String> dataList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        cityList = findViewById(R.id.city_list);
+
+        String []cities = {"Edmonton", "Vancouver"};
+         dataList = new ArrayList<>();
+         dataList.addAll(Arrays.asList(cities));
+
+         cityAdapter = new ArrayAdapter<>(this, R.layout.content, dataList);
+         cityList.setAdapter(cityAdapter);
+
+         Button add = findViewById(R.id.add_button);
+         Button delete = findViewById(R.id.delete_button);
+         EditText text = findViewById(R.id.text);
+
+         add.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 String string = text.getText().toString();
+                 dataList.add(string);
+                 cityAdapter.notifyDataSetChanged();
+             }
+         });
+
+         delete.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 String string = text.getText().toString();
+                 dataList.remove(string);
+                 cityAdapter.notifyDataSetChanged();
+             }
+         });
+
+         text.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+
+             }
+         });
+    }
+}
